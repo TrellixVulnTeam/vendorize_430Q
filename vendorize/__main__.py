461f687b1ad0daa5cb54cda9b2a46382a5c0e2ba
Christian Dywan <christian.dywan@canonical.com>
2018-03-19 17:30:46 +0100

    Use all Launchpad servers by default

--- vendorize/__main__.py
+++ vendorize/__main__.py
@@ -4,6 +4,13 @@ import os
 import vendorize
 
 
+_ALLOWED_HOSTS = [
+    'launchpad.net', 'keyserver.ubuntu.com', 'bazaar.launchpad.net',
+    'git.launchpad.net', 'api.launchpad.net', 'api.snapcraft.io',
+    'search.apps.ubuntu.com', 'archive.ubuntu.com', 'security.ubuntu.com'
+    ]
+
+
 @click.command()
 @click.version_option(version='0.1')
 @click.option('--dry-run', '-n', is_flag=True, help='Just verify what to do')
@@ -11,7 +18,7 @@ import vendorize
 @click.argument('project_folder',
                 type=click.Path(exists=True), default=os.getcwd())
 @click.option('target_branch', '-t', default=None, help='Target branch')
-@click.option('host', '-h', default=['git.launchpad.net'], help='Allowed host',
+@click.option('host', '-h', default=_ALLOWED_HOSTS, help='Allowed host',
               multiple=True)
 def run(dry_run, debug, project_folder, target_branch, host):
     os.chdir(project_folder)
