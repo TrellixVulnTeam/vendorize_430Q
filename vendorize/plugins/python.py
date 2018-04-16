826f1243a550a58738c8ba01d641d3f20528644e
Christian Dywan <christian@twotoasts.de>
2018-04-16 11:37:12 +0200

    Stop relying on being able to call pip API

--- vendorize/plugins/python.py
+++ vendorize/plugins/python.py
@@ -1,6 +1,7 @@
 import os
 import pip
 import setuptools
+import subprocess
 import shutil
 
 
@@ -13,7 +14,12 @@ class Python(vendorize.plugin.Plugin):
         super().__init__(*args, **kwargs)
         if os.getenv('SNAP_NAME') == 'vendorize':
             # Override user agent to avoid pip searching /etc for release files
-            pip.download.user_agent = lambda: 'pip/{}'.format(pip.__version__)
+            try:
+                user_agent = 'pip/{}'.format(pip.__version__)
+                pip.download.user_agent = lambda: user_agent
+            except AttributeError:
+                # 10.0.0 changes the API but also fixes the bug
+                pass
 
         self.python_cache = os.path.join(self.part_dir, 'python-packages')
 
@@ -47,11 +53,18 @@ class Python(vendorize.plugin.Plugin):
         self.debug('Fetching: {}'.format(', '.join(python_packages)))
         for package in python_packages:
             # Downloaded wheels/ archives are saved to the current folder
-            pip.main(['download', '--no-binary=:all:', '-q',
-                      '--exists-action=i',  # ignore
-                      '--dest={}'.format(self.python_cache),
-                      '--src={}'.format(self.python_cache)] +
-                     package.split(' '))
+            try:
+                subprocess.check_call([
+                    'python3', '-m', 'pip',
+                    'download', '--no-binary=:all:', '-q',
+                    '--exists-action=i',  # ignore
+                    '--dest={}'.format(self.python_cache),
+                    '--src={}'.format(self.python_cache)] +
+                    package.split(' '))
+            except subprocess.CalledProcessError as e:
+                # Errors in setup.py due to for example pkg-config being run
+                # can be ignored since we're not looking to build anything.
+                self.debug('Error during download: {!r}'.format(e))
 
     def unpack_archives(self):
         # Unpack all archives, skip folders of "editable" packages.
